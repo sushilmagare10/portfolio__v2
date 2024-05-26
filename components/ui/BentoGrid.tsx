252fcc77426918493b6e40d6ba154874688fd4b3
Sushil Magare <sushilmagare10@gmail.com>
2024-05-26 20:42:43 +0530

    "minor errors"

--- components/ui/BentoGrid.tsx
+++ components/ui/BentoGrid.tsx
@@ -1,18 +1,12 @@
 "use client"
 
 import { useState } from "react";
-
-// Also install this npm i --save-dev @types/react-lottie
 import Lottie from "react-lottie";
-
 import { cn } from "@/lib/utils";
-
-
+import { IoCopyOutline } from "react-icons/io5";
 import { BackgroundGradientAnimation } from "./GradientBg";
-import GridGlobe from "./GridGlobe";
 import animationData from "@/data/confetti.json";
 import MagicButton from "../MagicButton";
-import { SparklesCore } from "./sparkles";
 
 export const BentoGrid = ({
     className,
@@ -39,7 +33,6 @@ export const BentoGridItem = ({
     id,
     title,
     description,
-    //   remove unecessary things here
     img,
     imgClassName,
     titleClassName,
@@ -77,19 +70,15 @@ export const BentoGridItem = ({
     return (
         <div
             className={cn(
-                // remove p-4 rounded-3xl dark:bg-black dark:border-white/[0.2] bg-white  border border-transparent, add border border-white/[0.1] overflow-hidden relative
                 "  row-span-1 relative overflow-hidden rounded-xl border text-white border-white/[0.15] group/bento hover:shadow-xl transition duration-200 shadow-input dark:shadow-none justify-between flex flex-col space-y-4",
                 className
             )}
             style={{
-                //   add these two
-                //   you can generate the color from here https://cssgradient.io/
                 background: "rgb(4,7,29)",
                 backgroundColor:
                     "linear-gradient(90deg, rgba(4,7,29,1) 0%, rgba(12,14,35,1) 100%)",
             }}
         >
-            {/* add img divs */}
             <div className={`${id === 5 && "flex justify-center"} h-full`}>
                 <div className="w-full md:h-full h-[550px] absolute">
                     {img && (
@@ -108,13 +97,11 @@ export const BentoGridItem = ({
                         <img
                             src={spareImg}
                             alt={spareImg}
-                            //   width={220}
                             className="object-cover object-center w-full h-full"
                         />
                     )}
                 </div>
                 {id === 5 && (
-                    // add background animation , remove the p tag
                     <BackgroundGradientAnimation>
                         <div className="absolute z-50 inset-0 flex items-center justify-center text-white font-bold px-4 pointer-events-none text-3xl text-center md:text-4xl lg:text-7xl"></div>
                     </BackgroundGradientAnimation>
@@ -126,19 +113,15 @@ export const BentoGridItem = ({
                         "group-hover/bento:translate-x-2 transition duration-200 relative md:h-full min-h-40 flex flex-col px-5 p-5 lg:p-10"
                     )}
                 >
-                    {/* change the order of the title and des, font-extralight, remove text-xs text-neutral-600 dark:text-neutral-300 , change the text-color */}
                     <div className="font-sans font-extralight md:max-w-32 md:text-xs lg:text-base text-sm text-[#C1C2D3] z-10">
                         {description}
                     </div>
-                    {/* add text-3xl max-w-96 , remove text-neutral-600 dark:text-neutral-300*/}
-                    {/* remove mb-2 mt-2 */}
                     <div
                         className={`font-sans text-lg lg:text-3xl max-w-96 font-bold z-10`}
                     >
                         {title}
                     </div>
 
-                    {/* Tech stack list div */}
                     {id === 2 && (
                         <div className="flex gap-1 lg:gap-5 w-fit absolute -right-3 lg:-right-2">
                             {/* tech stack lists */}
@@ -170,10 +153,6 @@ export const BentoGridItem = ({
                     )}
                     {id === 5 && (
                         <div className="mt-5 relative">
-                            {/* button border magic from tailwind css buttons  */}
-                            {/* add rounded-md h-8 md:h-8, remove rounded-full */}
-                            {/* remove focus:ring-2 focus:ring-slate-400 focus:ring-offset-2 focus:ring-offset-slate-50 */}
-                            {/* add handleCopy() for the copy the text */}
                             <div
                                 className={`absolute -bottom-5 right-0 ${copied ? "block" : "block"
                                     }`}
@@ -185,6 +164,7 @@ export const BentoGridItem = ({
                             <MagicButton
                                 title={copied ? "Email is Copied!" : "Copy my email address"}
                                 position="left"
+                                icon={<IoCopyOutline />}
                                 handleClick={handleCopy}
                                 otherClasses="!bg-[#161A31]"
                             />
